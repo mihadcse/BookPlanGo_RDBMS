db6d58a8e15197301fdd72e7cde3022a96170911
mihadcse <huzzatullah@iut-dhaka.edu>
2025-04-28 14:46:04 +0600

    Add the FileChooser to the to choose image file in the Admin_Add_Place class

--- src/main/java/org/example/bookplango/Admin_Add_Place.java
+++ src/main/java/org/example/bookplango/Admin_Add_Place.java
@@ -16,6 +16,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javafx.stage.FileChooser;
+import java.io.File;
+
 public class Admin_Add_Place {
     private Stage stage;
     private Scene scene;
@@ -23,8 +26,9 @@ public class Admin_Add_Place {
     @FXML
     private Label success_label;
     @FXML
-    private TextField place_name_text,place_district_text,place_division_text,place_image_text;
+    private TextField place_name_text, place_district_text, place_division_text, place_image_text;
 
+    // Switch to Admin Dashboard
     public void switchtoAdminDashboardScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("admin_welcome.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -34,30 +38,34 @@ public class Admin_Add_Place {
         stage.show();
     }
 
+    // Select image file
+    @FXML
+    private void selectImageFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            place_image_text.setText(selectedFile.getAbsolutePath());
+        }
+    }
+
+    // Add place to database
     public void addPlace() throws SQLException {
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
 
-        PreparedStatement pstmt = connectDB.prepareStatement("INSERT INTO `bookplango`.`travel_places` (\n" +
-                "    `place_name`,\n" +
-                "    `place_district`,\n" +
-                "    `place_division`,\n" +
-                "    `place_image_path`\n" +
-                ")\n" +
-                "VALUES (\n" +
-                "    ?,\n" +
-                "    ?,\n" +
-                "    ?,\n" +
-                "    ?\n" +
-                ");");
+        PreparedStatement pstmt = connectDB.prepareStatement("INSERT INTO `bookplango`.`travel_places` (" +
+                "`place_name`, `place_district`, `place_division`, `place_image_path`) VALUES (?, ?, ?, ?);");
 
-        pstmt.setString(1,place_name_text.getText());
-        pstmt.setString(2,place_district_text.getText());
-        pstmt.setString(3,place_division_text.getText());
-        pstmt.setString(4,place_image_text.getText());
-        // Executing the statement
+        pstmt.setString(1, place_name_text.getText());
+        pstmt.setString(2, place_district_text.getText());
+        pstmt.setString(3, place_division_text.getText());
+        pstmt.setString(4, place_image_text.getText());
+
         pstmt.executeUpdate();
         System.out.println("Record inserted successfully");
         success_label.setText("Successfully Added");
     }
 }
+
